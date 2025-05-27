cc1ec4c8fdfdc9eb6c5031118eb2f934d1d8bcbc
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-27 20:13:34 +0600

    added mongodb connected

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const express =require('express');
-const cors =require('cors')
-const app =express();
-const port =process.env.PORT || 3000;
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("carrer code job portal getting the server")
-})
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.off1efx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.listen(port,()=>{
-    console.log(`server is getting start at:${port}`)
-})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("carrer code job portal getting the server");
+});
+
+app.listen(port, () => {
+  console.log(`server is getting start at:${port}`);
+});
