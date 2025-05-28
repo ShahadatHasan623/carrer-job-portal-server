1699b4e546df135ccd3510f4f0f733cabe0cf616
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-28 12:43:06 +0600

    added specefic jobId add to job application

--- index.js
+++ index.js
@@ -46,6 +46,24 @@ async function run() {
       // console.log(user)
       const result =await applicationsCollection.insertOne(user);
       res.send(result)
+    });
+    app.get("/applications",async(req,res)=>{
+      const email =req.query.email;
+      const query ={
+        applicant:email
+      }
+      const result =await applicationsCollection.find(query).toArray();
+
+      // bad way 
+      for(const application of result ){
+        const jobId =application.jobId;
+        const jobQuery ={_id: new ObjectId(jobId)}
+        const job =await jobsCollection.findOne(jobQuery)
+        application.company=job.company;
+        application.title=job.title;
+        application.company_logo =job.company_logo;
+      }
+      res.send(result)
     })
     await client.db("admin").command({ ping: 1 });
     console.log(
